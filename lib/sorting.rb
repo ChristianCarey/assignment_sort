465db95fe546842af51dd815c2e122793eb66a70
Christian Carey <christianrcarey@gmail.com>
2016-11-10 12:20:22 -0500

    Start merge sort

--- lib/sorting.rb
+++ lib/sorting.rb
@@ -27,5 +27,89 @@ def insertion_sort(array)
 end
 
 def bubble_sort(array)
-
+  
+  until array.sorted?
+    array.each_with_index do |element, index|
+      break if index == array.length - 1
+      array.swap(index, index +1 ) unless element <= array[index + 1]
+    end
+  end
+  array
 end
+
+class Array
+  def swap(first, second)
+    self[first], self[second] = self[second], self[first]
+    self
+  end
+
+  def sorted?
+    sorted = true
+    self.each_with_index do |element, index|
+      break if index == self.length - 1
+      sorted = false if self[index] > self[index + 1]
+    end
+    sorted
+  end
+end
+
+def merge_sort(array)
+  return array if array.length <= 1
+
+  midpoint = array.length / 2
+  left = array[0..midpoint]
+  right = array[midpoint + 1..-1]
+  left = merge_sort(left)
+  right = merge_sort(right)
+  merge(left, right)
+end
+
+def merge(left, right)
+  new_arr = []
+
+  until left.empty? || right.empty?
+    if left[0] < right[0]
+      new_arr.push(left[0])
+    else
+      new_arr.push(right[0])
+    end
+  end
+
+  # handle the last value
+  new_arr.push(left[0]) unless left.empty?
+  new_arr.push(right[0]) unless right.empty?
+  new_arr
+end
+
+# function merge(leftArr, rightArr){
+
+#     // var newArr = [];
+
+#     // compare leftArr[0] and rightArr[0]
+#     // whichever is smaller, push it onto newArr
+
+#     // repeat the process, but don't reuse elements,
+#     // until you have pushed all the elements from
+#     // leftArr and rightArr onto newArr in sorted order
+
+#     // return newArr
+# }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- spec/insertion_sort_spec.rb
+++ spec/insertion_sort_spec.rb
@@ -11,6 +11,30 @@ describe 'Sorting algorithms' do
   end
 
   describe 'bubble_sort' do
-    it 'should sort the array given it'
+    it 'should sort the array given it' do 
+      expect(bubble_sort(array)).to eq([1,2,3,5,7])
+    end
+  end
+
+  describe 'swap' do 
+    it "swaps two positions in an array" do 
+      expect(array.swap(2,3)).to eq([1,3,2,7,5])
+    end
+  end
+
+  describe "sorted?" do 
+    it "returns false for a non sorted array" do 
+      expect(array.sorted?).to be false
+    end
+
+    it "returns true for a sorted array" do 
+      expect([1,2,7,].sorted?).to be true
+    end
+  end
+
+  describe "merge_sort" do 
+    it 'should sort the array given it' do
+      expect(merge_sort(array)).to eq([1,2,3,5,7])
+    end
   end
 end
